88cfcc08ef4456d845f23b71050fbb1fedef4a43
yemengxuezhang <yemengxz@uci.edu>
2022-05-09 20:50:31 -0700

    Setting up the static file server to Heroku for further development

---
+++ server.js
@@ -0,0 +1,33 @@
+/************************/
+/* Set up the static file server */
+let static = require('node-static');
+
+/* Set up the http server library */
+let http = require('http');
+
+/* Assume that we are running on Heroku */
+let port = process.env.PORT;
+let directory = __dirname + '/public';
+
+/* if we aren't on Heroku, then we need to adjust our port and directory */
+
+if ((typeof port == 'undefined') || (port ===null)){
+    port = 8080;
+    directory = './public';
+}
+
+/* set up our static fiel web server to deliver files from the fielsystem */
+
+let file = new static.Server(directory);
+
+let app = http.createServer(
+    function(request,response){
+        request.addListener('end',
+          function(){
+               file.server(request,response);
+          }
+          ).resume();
+    }
+).listen(port);
+
+console.log('The server is running');
